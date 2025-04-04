fe8d0c2a1417eff8245e1d838a84126220db7d81
floliq <enboikovlad@gmail.com>
2025-04-04 09:55:12 +0300

    ITO-xx: Added .env settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: [
-      '.ngrok-free.app',
-    ],
-  },
+export default defineConfig(({ mode }) => {
+  loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: [
+        '.ngrok-free.app',
+      ],
+    },
+  }
 });
